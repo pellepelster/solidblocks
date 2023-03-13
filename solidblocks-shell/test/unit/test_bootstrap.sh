1835fdfe3341e3b46111ba80efbaff968e73dadf
Christian 'Pelle' Pelster <pelle@pelle.io>
2023-03-13 15:55:40 +0100

    fix installer tests

--- solidblocks-shell/test/unit/test_bootstrap.sh
+++ solidblocks-shell/test/unit/test_bootstrap.sh
@@ -6,17 +6,17 @@ set -eu -o pipefail
 
 source "${DIR}/utils.sh"
 
-#ls -lsa "${DIR}/../../examples/minimal-skeleton/do"
-
 
 ls -lsa "${DIR}/../../build/solidblocks-shell-${VERSION}.zip"
 
 mkdir -p "${TEMP_DIR}/pellepelster/solidblocks/releases/download/${VERSION}/"
 cp "${DIR}/../../build/solidblocks-shell-${VERSION}.zip" "${TEMP_DIR}/pellepelster/solidblocks/releases/download/${VERSION}/"
 
-docker run -it -d --rm --name nginx -p 8080:80 -v "${TEMP_DIR}:/usr/share/nginx/html" nginx
+docker run -it -d --rm --name nginx -p 8084:80 -v "${TEMP_DIR}:/usr/share/nginx/html" nginx
 
-SOLIDBLOCKS_BASE_URL="http://localhost:8080" "${DIR}/../../examples/minimal-skeleton/do" bootstrap
+sleep 2
+
+SOLIDBLOCKS_BASE_URL="http://localhost:8084" "${DIR}/../../build/documentation/shell_minimal_skeleton_do" bootstrap
 
 function clean() {
   clean_temp_dir
