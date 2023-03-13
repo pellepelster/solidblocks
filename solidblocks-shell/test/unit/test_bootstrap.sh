b312c948c86aa702381afe564996b9326d042af1
Christian 'Pelle' Pelster <pelle@pelle.io>
2023-03-13 16:03:08 +0100

    do not use static port for tests

--- solidblocks-shell/test/unit/test_bootstrap.sh
+++ solidblocks-shell/test/unit/test_bootstrap.sh
@@ -5,6 +5,7 @@ DIR="$(cd "$(dirname "$0")" ; pwd -P)"
 set -eu -o pipefail
 
 source "${DIR}/utils.sh"
+source "${DIR}/../../lib/docker.sh"
 
 
 ls -lsa "${DIR}/../../build/solidblocks-shell-${VERSION}.zip"
@@ -12,15 +13,17 @@ ls -lsa "${DIR}/../../build/solidblocks-shell-${VERSION}.zip"
 mkdir -p "${TEMP_DIR}/pellepelster/solidblocks/releases/download/${VERSION}/"
 cp "${DIR}/../../build/solidblocks-shell-${VERSION}.zip" "${TEMP_DIR}/pellepelster/solidblocks/releases/download/${VERSION}/"
 
-docker run -it -d --rm --name nginx -p 8084:80 -v "${TEMP_DIR}:/usr/share/nginx/html" nginx
+TEST_ID="bootstrap_$(uuidgen)"
+
+docker run -it -d --rm --name "${TEST_ID}" -p 80 -v "${TEMP_DIR}:/usr/share/nginx/html" nginx
 
 sleep 2
 
-SOLIDBLOCKS_BASE_URL="http://localhost:8084" "${DIR}/../../build/documentation/shell_minimal_skeleton_do" bootstrap
+SOLIDBLOCKS_BASE_URL="http://localhost:$(docker_mapped_tcp_port "${TEST_ID}" "80")" "${DIR}/../../build/documentation/shell_minimal_skeleton_do" bootstrap
 
 function clean() {
   clean_temp_dir
-  docker rm -f nginx
+  docker rm -f "${TEST_ID}"
 }
 
 trap clean HUP INT QUIT TERM EXIT
