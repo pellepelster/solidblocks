8b57f70e4f79e3e07de5383a342a52d26373d8d8
Christian 'Pelle' Pelster <pelle@pelle.io>
2023-04-20 21:38:40 +0200

    add hetzner module to build pipeline

--- solidblocks-hetzner/test/terraform/s3/main.tf
+++ solidblocks-hetzner/test/terraform/s3/main.tf
@@ -3,19 +3,25 @@ resource "aws_s3_bucket" "bootstrap" {
   force_destroy = true
 }
 
+locals {
+  base_path         = "${path.module}/../../../../solidblocks-cloud-init/"
+  bootstrap_zip     = tolist(fileset(local.base_path, "**/solidblocks-cloud-init-${var.solidblocks_version}.zip"))[0]
+  bootstrap_snippet = tolist(fileset(local.base_path, "**/cloud_init_bootstrap_solidblocks"))[0]
+}
+
 resource "aws_s3_object" "bootstrap_zip" {
   bucket = aws_s3_bucket.bootstrap.id
   key    = "pellepelster/solidblocks/releases/download/${var.solidblocks_version}/solidblocks-cloud-init-${var.solidblocks_version}.zip"
-  source = "${path.module}/../../../../solidblocks-cloud-init/build/solidblocks-cloud-init-${var.solidblocks_version}.zip"
+  source = "${local.base_path}/${local.bootstrap_zip}"
   acl    = "public-read"
-  etag   = filemd5("${path.module}/../../../../solidblocks-cloud-init/build/solidblocks-cloud-init-${var.solidblocks_version}.zip")
+  etag   = filemd5("${local.base_path}/${local.bootstrap_zip}")
 }
 
 resource "aws_s3_object" "bootstrap_snippet" {
   bucket = aws_s3_bucket.bootstrap.id
   key    = "pellepelster/solidblocks/releases/download/${var.solidblocks_version}/cloud_init_bootstrap_solidblocks"
-  source = "${path.module}/../../../../solidblocks-cloud-init/build/snippets/cloud_init_bootstrap_solidblocks"
+  source = "${local.base_path}/${local.bootstrap_snippet}"
   acl    = "public-read"
-  etag   = filemd5("${path.module}/../../../../solidblocks-cloud-init/build/snippets/cloud_init_bootstrap_solidblocks")
+  etag   = filemd5("${local.base_path}/${local.bootstrap_snippet}")
 }
 
