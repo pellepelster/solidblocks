0c9c88f7818f8317abffcca3561c1afd7b2f502f
Christian 'Pelle' Pelster <pelle@pelle.io>
2023-07-07 08:38:14 +0200

    bump version to v0.1.20

--- solidblocks-hetzner/modules/rds-postgresql/variables.tf
+++ solidblocks-hetzner/modules/rds-postgresql/variables.tf
@@ -181,12 +181,12 @@ variable "solidblocks_base_url" {
 variable "solidblocks_cloud_init_version" {
   type        = string
   description = "used for integration tests to inject test versions"
-  default     = "v0.1.17"
+  default     = "v0.1.19"
 }
 
 variable "solidblocks_rds_version" {
   type        = string
   description = "used for integration tests to inject test versions"
-  default     = "v0.1.17"
+  default     = "v0.1.19"
 }
 
