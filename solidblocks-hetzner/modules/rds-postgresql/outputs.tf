443481ddc62cb2a540937e2457e6f20ee521eb31
Julian Tölle <julian.toelle97@gmail.com>
2023-09-23 22:40:59 +0200

    feat: output for ipv6 address of postgres server
    To allow setting up AAAA DNS Records.

--- solidblocks-hetzner/modules/rds-postgresql/outputs.tf
+++ solidblocks-hetzner/modules/rds-postgresql/outputs.tf
@@ -1,11 +1,16 @@
 output "ipv4_address" {
   value       = hcloud_server.rds.ipv4_address
-  description = "IpV4 address of the created server if applicable"
+  description = "IPv4 address of the created server if applicable"
+}
+
+output "ipv6_address" {
+  value       = hcloud_server.rds.ipv6_address
+  description = "IPv6 address of the created server if applicable"
 }
 
 output "ipv4_address_private" {
   value       = try(one(hcloud_server.rds.network).ip, null)
-  description = "private IpV4 address of the created server if applicable"
+  description = "private IPv4 address of the created server if applicable"
 }
 
 output "this_server_id" {
