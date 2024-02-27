5e7c5cf9e6c59c027cc53fe3bb8a7c2846563e67
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-02-27 20:26:13 +0100

    expose restore mode as environment variable

--- solidblocks-hetzner/modules/rds-postgresql/user_data.sh
+++ solidblocks-hetzner/modules/rds-postgresql/user_data.sh
@@ -139,6 +139,9 @@ services:
       - "DB_USERNAME_${database.id}=${database.user}"
       - "DB_PASSWORD_${database.id}=${database.password}"
       %{~ endfor ~}
+      %{~ if mode != "" ~}
+      DB_MODE: "${mode}"
+      %{~ endif ~}
       %{~ if db_admin_password != "" ~}
       - "DB_ADMIN_PASSWORD=${db_admin_password}"
       %{~ endif ~}
