1aba8b9cdd24706b05fa2669e85ae715ef2156eb
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-07-05 21:22:28 +0200

    remove duplicate test

--- solidblocks-hetzner/test/test_ssl.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-def test_ssl_certificates(host):
-    assert host.file(f"/storage/data/ssl/certificates/test3.blcks.de.crt").exists
-    assert host.file(f"/storage/data/ssl/certificates/test3.blcks.de.key").exists
-
-
