2f9008889782680c53d9a67fd25ce8876408ae47
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-07-18 11:40:53 +0200

    prepare release

--- solidblocks-hetzner/modules/rds-postgresql/variables.tf
+++ solidblocks-hetzner/modules/rds-postgresql/variables.tf
@@ -297,11 +297,11 @@ variable "solidblocks_base_url" {
 variable "solidblocks_cloud_init_version" {
   type        = string
   description = "Solidblocks cloud-init version to use"
-  default     = "1.2.6-pre4" #solidblocks_cloud_init_version
+  default     = "1.2.6-pre5" #solidblocks_cloud_init_version
 }
 
 variable "solidblocks_rds_version" {
   type        = string
   description = "Solidblocks rds-postgresql version to use"
-  default     = "1.2.6-pre4" #solidblocks_rds_version
+  default     = "1.2.6-pre5" #solidblocks_rds_version
 }
--- solidblocks-hetzner/snippets/hetzner-postgres-rds-s3-backup/instance/main.tf
+++ solidblocks-hetzner/snippets/hetzner-postgres-rds-s3-backup/instance/main.tf
@@ -18,7 +18,7 @@ resource "hcloud_ssh_key" "ssh_key" {
 
 module "rds-postgresql" {
   source  = "pellepelster/solidblocks-rds-postgresql/hcloud"
-  version = "1.2.6-pre4"
+  version = "1.2.6-pre5"
 
   name     = "rds-postgresql"
   location = var.hetzner_location
