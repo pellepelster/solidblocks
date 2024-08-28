b9136ce831da2dc0e140662d2c73a4a7f305fb33
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-08-28 16:17:22 +0200

    add test snippets

---
+++ solidblocks-test/snippets/test-gradle/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+    implementation("de.solidblocks:infra-test:v0.2.7-pre1")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
