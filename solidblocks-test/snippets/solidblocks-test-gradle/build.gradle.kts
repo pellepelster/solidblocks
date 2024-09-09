98ae28614072e61a7935c06155fff797cb631875
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-09-09 16:29:53 +0200

    prepare release

--- solidblocks-test/snippets/solidblocks-test-gradle/build.gradle.kts
+++ solidblocks-test/snippets/solidblocks-test-gradle/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
     implementation("org.junit.jupiter:junit-jupiter-engine:5.11.0")
-    implementation("de.solidblocks:infra-test:0.2.8-pre8")
+    implementation("de.solidblocks:infra-test:0.2.8-pre9")
 }
 
 tasks.named<Test>("test") {
