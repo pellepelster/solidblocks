0e0d61da0433b1691c0a054c29629ce2a57e8930
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-09-09 14:09:13 +0200

    prepare release

--- solidblocks-hetzner/snippets/hetzner-postgres-rds-s3-backup/instance/main.tf
+++ solidblocks-hetzner/snippets/hetzner-postgres-rds-s3-backup/instance/main.tf
@@ -18,7 +18,7 @@ resource "hcloud_ssh_key" "ssh_key" {
 
 module "rds-postgresql" {
   source  = "pellepelster/solidblocks-rds-postgresql/hcloud"
-  version = "0.2.8-pre5"
+  version = "0.2.8-pre7"
 
   name     = "rds-postgresql"
   location = var.hetzner_location
