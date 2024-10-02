e8929ff332e2d4191e6bbd3d273b8c22e2ad5f18
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-10-02 13:59:19 +0200

    prepare release

--- solidblocks-cloud-init/snippets/cloud-init-kitchen-sink/main.tf
+++ solidblocks-cloud-init/snippets/cloud-init-kitchen-sink/main.tf
@@ -11,7 +11,7 @@ resource "hcloud_volume_attachment" "example" {
 }
 
 module "example" {
-  source  = "github.com/pellepelster/terraform-null-solidblocks-cloud-init?ref=0.2.9-pre2"
+  source  = "github.com/pellepelster/terraform-null-solidblocks-cloud-init?ref=0.2.9-pre3"
 
   storage = [
     { linux_device = hcloud_volume.example.linux_device, mount_path = "/data1" },
