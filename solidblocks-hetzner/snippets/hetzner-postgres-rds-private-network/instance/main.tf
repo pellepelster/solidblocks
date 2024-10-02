292fe6facf9bd070d99edbc81b6c53c8ac99d5ba
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-10-02 14:39:02 +0200

    prepare release

--- solidblocks-hetzner/snippets/hetzner-postgres-rds-private-network/instance/main.tf
+++ solidblocks-hetzner/snippets/hetzner-postgres-rds-private-network/instance/main.tf
@@ -30,7 +30,7 @@ resource "hcloud_network_subnet" "subnet" {
 
 module "rds-postgresql" {
   source  = "pellepelster/solidblocks-rds-postgresql/hcloud"
-  version = "0.2.9-pre3"
+  version = "0.2.9-pre4"
 
   name     = "rds-postgresql"
   location = var.hetzner_location
