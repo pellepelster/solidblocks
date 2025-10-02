e6dfc7cac488c695fdc48062e789aa808e0270f8
Christian 'Pelle' Pelster <pelle@pelle.io>
2025-10-02 22:00:24 +0200

    fix GCP integration tests

--- solidblocks-rds-postgresql-docker/src/test/kotlin/de/solidblocks/rds/postgresql/test/extensions/GcsTestBedExtension.kt
+++ solidblocks-rds-postgresql-docker/src/test/kotlin/de/solidblocks/rds/postgresql/test/extensions/GcsTestBedExtension.kt
@@ -1,5 +1,6 @@
 package de.solidblocks.rds.postgresql.test.extensions
 
+import com.google.auth.oauth2.ServiceAccountCredentials
 import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
 import kotlinx.coroutines.async
@@ -7,54 +8,60 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.extension.*
 
+
 class GcsTestBedExtension : ParameterResolver, AfterEachCallback {
 
-  val testBeds = mutableMapOf<String, GcsTestBed>()
+    val testBeds = mutableMapOf<String, GcsTestBed>()
 
-  val storageClient = StorageOptions.newBuilder().setProjectId("solidblocks-test").build().service
+    val key = System.getenv("GCP_SERVICE_ACCOUNT_KEY")
+    var credential =
+        ServiceAccountCredentials.fromStream(key.byteInputStream())
 
-  override fun supportsParameter(
-      parameterContext: ParameterContext,
-      extensionContext: ExtensionContext,
-  ): Boolean = parameterContext.parameter.type == GcsTestBed::class.java
+    val storageClient =
+        StorageOptions.newBuilder().setCredentials(credential).setProjectId("solidblocks-test").build().service
 
-  @Throws(ParameterResolutionException::class)
-  override fun resolveParameter(
-      parameterContext: ParameterContext,
-      context: ExtensionContext,
-  ): Any = createTestBed(context)
+    override fun supportsParameter(
+        parameterContext: ParameterContext,
+        extensionContext: ExtensionContext,
+    ): Boolean = parameterContext.parameter.type == GcsTestBed::class.java
 
-  override fun afterEach(context: ExtensionContext) {
-    // testBeds[context.uniqueId]?.destroyTestBed()
-  }
+    @Throws(ParameterResolutionException::class)
+    override fun resolveParameter(
+        parameterContext: ParameterContext,
+        context: ExtensionContext,
+    ): Any = createTestBed(context)
 
-  private fun createTestBed(context: ExtensionContext): GcsTestBed =
-      testBeds.getOrPut(context.uniqueId) { GcsTestBed(storageClient) }.also { it.initTestbed() }
-
-  private fun removeAllTestBuckets() {
-    storageClient.list(Storage.BucketListOption.prefix("test-")).streamAll().forEach { bucket ->
-      logger.info { "[test] deleting all objects for bucket '${bucket.name}'" }
-      runBlocking {
-        while (true) {
-          val blobs = storageClient.get(bucket.name).list().values.toList()
-
-          if (blobs.isEmpty()) {
-            break
-          }
-
-          blobs
-              .map { blob ->
-                async {
-                  logger.info { "[test] deleting blob '${blob.name}' for bucket '${bucket.name}'" }
-                  blob.delete()
-                }
-              }
-              .awaitAll()
-        }
-      }
-
-      logger.info { "[test] deleting bucket '${bucket.name}'" }
-      storageClient.get(bucket.name).delete()
+    override fun afterEach(context: ExtensionContext) {
+        // testBeds[context.uniqueId]?.destroyTestBed()
     }
-  }
+
+    private fun createTestBed(context: ExtensionContext): GcsTestBed =
+        testBeds.getOrPut(context.uniqueId) { GcsTestBed(storageClient) }.also { it.initTestbed() }
+
+    private fun removeAllTestBuckets() {
+        storageClient.list(Storage.BucketListOption.prefix("test-")).streamAll().forEach { bucket ->
+            logger.info { "[test] deleting all objects for bucket '${bucket.name}'" }
+            runBlocking {
+                while (true) {
+                    val blobs = storageClient.get(bucket.name).list().values.toList()
+
+                    if (blobs.isEmpty()) {
+                        break
+                    }
+
+                    blobs
+                        .map { blob ->
+                            async {
+                                logger.info { "[test] deleting blob '${blob.name}' for bucket '${bucket.name}'" }
+                                blob.delete()
+                            }
+                        }
+                        .awaitAll()
+                }
+            }
+
+            logger.info { "[test] deleting bucket '${bucket.name}'" }
+            storageClient.get(bucket.name).delete()
+        }
+    }
 }
