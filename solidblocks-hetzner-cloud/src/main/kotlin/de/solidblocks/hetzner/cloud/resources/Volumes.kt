04228674aab9ea9582eb19ad42cbc94eb3256162
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-02-13 12:14:03 +0100

    remove unused class

--- solidblocks-hetzner-cloud/src/main/kotlin/de/solidblocks/hetzner/cloud/resources/Volumes.kt
+++ solidblocks-hetzner-cloud/src/main/kotlin/de/solidblocks/hetzner/cloud/resources/Volumes.kt
@@ -4,24 +4,8 @@ import de.solidblocks.hetzner.cloud.*
 import de.solidblocks.hetzner.cloud.model.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-
-object VolumeFormatNullSerializer : KSerializer<VolumeFormat?> {
-
-  override val descriptor: SerialDescriptor =
-      PrimitiveSerialDescriptor("VolumeFormatNull", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: VolumeFormat?) {}
-
-  override fun deserialize(decoder: Decoder): VolumeFormat? = null
-}
 
 @Suppress("ktlint:standard:enum-entry-name-case")
 enum class VolumeFormat {
