7e8290e89595f732d6c3fdd7a688bb7ae5b166d8
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-02-13 12:20:29 +0100

    remove do file

---
+++ solidblocks-debug-container/src/test/kotlin/de/solidblocks/debug/container/DebugContainerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.solidblocks.debug.container
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DebugContainerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
