4c622f0dc04e90d0b0b322096ebcf6496466222f
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-02-16 20:24:10 +0100

    update gradle plugins

--- solidblocks-debug-container/build.gradle.kts
+++ solidblocks-debug-container/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("buildlogic.solidblocks-kotlin-conventions")
-    id("org.springframework.boot") version "3.3.3"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "4.0.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 repositories {
