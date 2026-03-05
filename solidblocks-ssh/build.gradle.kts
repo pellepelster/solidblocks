0c5d9aeb7fc624ba2b5e36571fa051f79e85e9db
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-03-05 07:59:45 +0100

    add maven artifact publishing for ssh

--- solidblocks-ssh/build.gradle.kts
+++ solidblocks-ssh/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("buildlogic.solidblocks-kotlin-conventions")
     id("com.vanniktech.maven.publish") version "0.29.0"
@@ -19,4 +21,39 @@ dependencies {
 
 mavenPublishing {
     coordinates("de.solidblocks", "ssh", "${version}")
+
+    pom {
+        name.set("Solidblocks Test")
+        description.set("Solidblocks is a library of reusable components for infrastructure operation, automation and developer experience")
+        inceptionYear.set("2019")
+        url.set("https://github.com/pellepelster/solidblocks/")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("https://opensource.org/license/mit")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("pellepelster")
+                name.set("Christian 'Pelle' Pelster")
+                url.set("https://github.com/pellepelster/")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/pellepelster/solidblocks/")
+            connection.set("scm:git:git://github.com/pellepelster/solidblocks.git")
+            developerConnection.set("scm:git:ssh://git@github.com/pellepelster/solidblocks.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    if (!project.gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal") }) {
+        signAllPublications()
+    }
 }
