364d3c55dde823d1af59c87919505b3cbbf80b12
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-03-05 06:41:36 +0100

    prepeare release

--- solidblocks-hetzner-cloud/snippets/solidblocks-hetzner-cloud-example/settings.gradle.kts
+++ solidblocks-hetzner-cloud/snippets/solidblocks-hetzner-cloud-example/settings.gradle.kts
@@ -2,12 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "solidblocks-test-gradle"
-
-/*
-includeBuild("../../../") {
-    dependencySubstitution {
-        substitute(module("de.solidblocks:infra-test")).using(project(":solidblocks-test"))
-    }
-}
-*/
+rootProject.name = "solidblocks-hetzner-cloud-example"
