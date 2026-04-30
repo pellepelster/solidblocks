e05b890897a892750b13a10032420110614222aa
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-04-30 17:28:32 +0200

    run cloud-init tests parallel

--- solidblocks-cloud-init/build.gradle.kts
+++ solidblocks-cloud-init/build.gradle.kts
@@ -126,7 +126,7 @@ tasks.test {
     systemProperty("junit.jupiter.execution.parallel.enabled", "true")
     systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
     systemProperty("junit.jupiter.execution.parallel.config.strategy", "fixed")
-    systemProperty("junit.jupiter.execution.parallel.config.fixed.parallelism", "3")
+    systemProperty("junit.jupiter.execution.parallel.config.fixed.parallelism", "4")
 }
 
 tasks.getByName("jar").dependsOn(generate)
