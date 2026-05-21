2e2a78c8381416e67017123c527692f982a9a7d8
Christian 'Pelle' Pelster <pelle@pelle.io>
2026-05-21 13:37:20 +0200

    disable nuke tests in ci

--- solidblocks-cli/src/integrationTest/kotlin/de/solidblocks/cli/NukeIntegrationTest.kt
+++ solidblocks-cli/src/integrationTest/kotlin/de/solidblocks/cli/NukeIntegrationTest.kt
@@ -5,6 +5,7 @@ import de.solidblocks.infra.test.SolidblocksTestContext
 import de.solidblocks.infra.test.assertions.shouldHaveExitCode
 import io.kotest.assertions.assertSoftly
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 import org.junit.jupiter.api.extension.ExtendWith
 import java.nio.file.Path
 import kotlin.time.Duration.Companion.minutes
@@ -14,6 +15,7 @@ class NukeIntegrationTest {
     val blcksCommand = Path.of(".").resolve("blcks")
 
     @Test
+    @DisabledIfEnvironmentVariable(named = "SKIP_TESTS", matches = ".*integration.*")
     fun testNuke(context: SolidblocksTestContext) {
         val testbed = Path.of(NukeIntegrationTest::class.java.getResource("/nuke-testbed").toURI())
 
